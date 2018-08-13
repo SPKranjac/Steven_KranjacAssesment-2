a8e9951b77b69ea92edcb04c5833e41bef631b12
SPKranjac <41457092+SPKranjac@users.noreply.github.com>
2018-08-13 17:34:58 +1000

    Update
    Updated enemy to patrol

---
+++ A2_GameTutorial/Assets/_Scripts/Boulder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boulder : MonoBehaviour {
+
+
+    void OncollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Destroy(this.gameObject);
+            Debug.Log(" boulder hit ");
+
+        } 
+    }
+}
---
+++ A2_GameTutorial/Assets/_Scripts/Patrol.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    //setting variables for speed, movement and ground detection for enemy character
+    public float speed;
+    private bool movingRight = true;
+    public Transform groundDetection;
+
+    void Update()
+    {
+        // Setting patrol for the enemy character
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position,Vector2.down, 5f);
+
+        if (groundInfo.collider == false);
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+}
