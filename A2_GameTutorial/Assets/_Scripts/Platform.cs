cd1dbd2c5741f433f7adf9ede9ae535341a76d23
SPKranjac <41457092+SPKranjac@users.noreply.github.com>
2018-08-14 22:15:26 +1000

    Updated
    Player now sticks to moving

---
+++ A2_GameTutorial/Assets/_Scripts/Platform.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+
+
+    // Setting the player to be parented to the moving platform
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+            collider.transform.SetParent(transform, true);
+        Debug.Log(" player on is the platform ");
+    }
+
+    void OnCollisionExit2D(Collision2D collider)
+    {
+        // making the player unparented from the platform
+        collider.transform.SetParent(transform.parent, true);
+        Debug.Log(" player is off the platform ");
+    }
+}
